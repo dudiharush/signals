a73b773a41343ca553fe6e19e78004a061415d51
David Harush <davidharush@gmail.com>
2023-07-30 08:02:37 +0300

    add tests

--- src/createMemo.spec.ts
+++ src/createMemo.spec.ts
@@ -4,30 +4,43 @@ import { createEffect } from "./createEffect";
 import { createMemo } from "./createMemo";
 
 it("check createEffect is being called ones on creation", () => {
+  console.log("1. Create");
+  const firstNameSignal = createSignal("John");
+  const lastNameNameSignal = createSignal("Smith");
+  const getFirstNameSpy = vi.spyOn(firstNameSignal, '0');
+  const getLastNameSpy = vi.spyOn(lastNameNameSignal, '0');
 
-console.log("1. Create");
-const [firstName, setFirstName] = createSignal("John");
-const [lastName, setLastName] = createSignal("Smith");
-const [showFullName, setShowFullName] = createSignal(true);
+  const [firstName, setFirstName] = firstNameSignal;
+  const [lastName, setLastName] = lastNameNameSignal;
+
+  const [showFullName, setShowFullName] = createSignal(true);
+
+  const calculateDisplayName = () => {
+    if (!showFullName()) return firstName();
+    return `${firstName()} ${lastName()}`;
+  };
+
+  const displayName = createMemo(calculateDisplayName);
+
+  createEffect(() => console.log("My name is", displayName()));
+
+  expect(getFirstNameSpy).toBeCalledTimes(1);
+  expect(getLastNameSpy).toBeCalledTimes(1);
+
+  console.log("2. Set showFullName: false ");
+  setShowFullName(false);
+  expect(getFirstNameSpy).toBeCalledTimes(2);
+  expect(getLastNameSpy).toBeCalledTimes(1);
 
 
-const displayName = createMemo(() => {
-  if (!showFullName()) return firstName();
-  return `${firstName()} ${lastName()}`
+  console.log("3. Change lastName");
+  setLastName("Legend");
+  expect(getFirstNameSpy).toBeCalledTimes(2);
+  expect(getLastNameSpy).toBeCalledTimes(1);
+
+  console.log("4. Set showFullName: true");
+  setShowFullName(true);
+  expect(getFirstNameSpy).toBeCalledTimes(3);
+  expect(getLastNameSpy).toBeCalledTimes(2);
+
 });
-
-createEffect(() => console.log("My name is", displayName()));
-
-console.log("2. Set showFullName: false ");
-setShowFullName(false);
-
-console.log("3. Change lastName");
-setLastName("Legend");
-
-console.log("4. Set showFullName: true");
-setShowFullName(true);
-
-expect(true).toEqual(true);
-
-
-})
